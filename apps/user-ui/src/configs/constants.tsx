cdf7f9190b226a4b3fe38f716b2ef8d583ddf10b
Tweenwrld <154606522+Tweenwrld@users.noreply.github.com>
2025-07-24 16:22:33 +0300

    initial commit

---
+++ apps/user-ui/src/configs/constants.tsx
@@ -0,0 +1,24 @@
+
+
+export const navItems: NavItemsTypes[] = [
+    { 
+        title: 'Home', 
+        href: '/' 
+    },
+    { 
+        title: 'Products', 
+        href: '/products' 
+    },
+    { 
+        title: 'Shops', 
+        href: '/shops' 
+    },
+    { 
+        title: 'Offers', 
+        href: '/offers' 
+    },
+    { 
+        title: 'Become a Seller', 
+        href: '/become-seller' 
+    }
+]
